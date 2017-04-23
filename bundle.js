c4ec56b477a7ed3150b5a8270b44cfaaa494ec0e
zcy <zurl@live.com>
2017-04-23 17:01:47 +0800

    make web version

--- bundle.js
+++ bundle.js
@@ -1,3 +1,488 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+
+function applyRegister(ctx){
+    if(ctx.exptreg.length == 0){
+        ctx.error(0, 'tvariable are not enough');
+        return null;
+    }
+    const dst = ctx.exptreg.pop();
+    ctx.exptregu[dst] = true;
+    return dst;
+}
+function freeRegister(ctx, x){
+    if(ctx.exptregu[x] == true){
+        ctx.exptregu[x] = false;
+        ctx.exptreg.push(x);
+    }
+}
+
+function lookup(ctx, name){
+    if(ctx.alias.hasOwnProperty(name))return ctx.alias[name];
+    if(ctx.sregs.hasOwnProperty(name))return ctx.sregs[name];
+    if(ctx.tregs.hasOwnProperty(name))return ctx.tregs[name];
+    if(ctx.locals.hasOwnProperty(name))return ctx.locals[name];
+    if(ctx.params.hasOwnProperty(name))return ctx.params[name];
+    return null;
+}
+
+exports.freeRegister = freeRegister;
+exports.lookup = lookup;
+exports.applyRegister = applyRegister;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const spiltRegex = /\s*(=|\+|(-?[0-9]+)|\*|-|\(|\)|\[|\]|\$?[a-zA-Z0-9_]+)\s*/g;
+const common = __webpack_require__(0);
+const applyRegister = common.applyRegister;
+const freeRegister = common.freeRegister;
+const lookup = common.lookup;
+
+function tokenize(str){
+    let match;
+    const result = [];
+    while(match = spiltRegex.exec(str)) result.push(match[1]);
+    return result;
+}
+// | ^ & ( < > ) (+ -) (*)
+const nextTable = { 'd': '|', '|': '^', '^':'&', '&':'<','<':'+','+':'*','*':'0'};
+function parseExpression(ctx, exp, target){
+    if(target == 'd'){
+        if(exp[ctx.cpos] == '*'){
+            ctx.cpos++;
+            return ['d', parseExpression(ctx, exp, '0')];
+        }
+        else{
+            return parseExpression(ctx, exp, '|');
+        }
+    }
+    let result = null;
+    let status = 0; // 0 for lhs 1 for ope 2 for rhs
+    let ope = null;
+    while(ctx.cpos < exp.length){
+        if(status == 0 || status == 2){
+            let buf = null;
+            if(target == '0'){
+                if(exp[ctx.cpos] == '('){
+                    ctx.cpos++;
+                    const tmp = parseExpression(ctx, exp, 'd');
+                    if(exp[ctx.cpos] != ')')return null;
+                    buf = tmp;
+                    ctx.cpos++;
+                    return buf;
+                }
+                else{
+                    let ret = null;
+                    if(exp[ctx.cpos].charAt(0) == '$'){
+                        ctx.cpos++;
+                        ret = exp[ctx.cpos-1];
+                    }
+                    else{
+                        let tmp = lookup(ctx, exp[ctx.cpos]);
+                        if(!tmp){
+                            if(!isNaN(parseInt(exp[ctx.cpos]))){
+                                ctx.cpos++;
+                                ret = parseInt(exp[ctx.cpos-1]);
+                            }else return null;
+                        }
+                        else{
+                            if(typeof(tmp) == 'number'){
+                                ret = ['l', tmp];
+                            }else{
+                                ctx.cpos++;
+                                ret = tmp;
+                            }
+                        }
+                    }
+                    if(exp[ctx.cpos] == '['){
+                        ctx.cpos++;
+                        const expr = parseExpression(ctx, exp, 'd');
+                        if(exp[ctx.cpos] != ']')return null;
+                        ctx.cpos++;
+                        return ['a', ret, expr];
+                    }
+                    else{
+                        return ret;
+                    }
+                }
+            }else{
+                buf = parseExpression(ctx, exp, nextTable[target]);
+            }
+            if(status == 0)result = buf;
+            else result.push(buf);
+            status = 1;
+        }
+        else{
+            if(target == '*'){
+                if(exp[ctx.cpos] == '*'){
+                    result = ['*', result];
+                }else break;
+            }
+            else if( target == '+'){
+                if(exp[ctx.cpos] == '+'){
+                    result = ['+', result];
+                }else if(exp[ctx.cpos] == '-'){
+                    result = ['-', result];
+                }else break;
+            }
+            else if( target == '<'){
+                if(exp[ctx.cpos] == '>'){
+                    result = ['>', result];
+                }else if(exp[ctx.cpos] == '<'){
+                    result = ['<', result];
+                }else break;
+            }else if( target == '&'){
+                if(exp[ctx.cpos] == '&'){
+                    result = ['&', result];
+                }else break;
+            }else if( target == '^'){
+                if(exp[ctx.cpos] == '^'){
+                    result = ['^', result];
+                }else break;
+            }else if( target == '|'){
+                if(exp[ctx.cpos] == '|'){
+                    result = ['|', result];
+                }else   break;
+            }
+            ctx.cpos++;
+            status = 2;
+        }
+    }
+    if(status == 1){
+        return result;
+    }else return null;
+}
+
+function parseAssignment(ctx, exp){
+    if(exp[1] != '=' ) return null;
+    if(exp[0].charAt(0) != '$'){
+        exp[0] = lookup(ctx, exp[0]);
+        if(!exp[0])return null;
+    }
+    ctx.cpos = 2;
+    const ast = parseExpression(ctx, exp, 'd');
+    if(!ast)return null;
+    return ['=', exp[0], ast];
+}
+const operatorMap = {
+    '+':'add', '-': 'sub', '&': 'and',
+    '-':'sub',
+    '|': 'or', '^': 'xor', '<': 'slt'};
+
+
+function pr(x){
+    if(typeof(x) == 'object')return "[" + x.map(y=>pr(y)).join(',') + "]";
+    else return x;
+}
+function generateExpression(ctx, ast){
+    if(typeof(ast) == 'number')return ast;
+    if(typeof(ast) == 'string')return ast;
+    const l = generateExpression(ctx, ast[1]);
+    if(ast[0] == 'd'){
+        if(typeof(l) == 'number'){
+            const dst = applyRegister(ctx);
+            ctx.buffer.push(`    lw ${dst}, ${l}($zero)`);
+            return dst;
+        }
+        else{
+            let dst = l;
+            if(ctx.exptregu[l] != true){
+                dst = applyRegister(ctx);;
+            }
+            ctx.buffer.push(`    lw ${dst}, 0(${dst})`);
+            return dst;
+        }
+    }
+    else if( ast[0] == 'l'){
+        const dst = applyRegister(ctx);
+        ctx.buffer.push(`    lw ${dst}, ${l}($sp)`);
+        return dst;
+    }
+    const r = generateExpression(ctx, ast[2]);
+    if(ast[0] == '*'){
+        if(typeof(l) == 'string' && typeof(r) == 'string'){
+            ctx.error(0, 'variable multiplication is not supported currently');
+            return null;
+        }
+        else if(typeof(l) == 'number' && typeof(r) == 'number'){
+            return l*r;
+        }
+        else {
+            const sr = applyRegister(ctx);
+            const tr = applyRegister(ctx);
+            let v,n;let neg = 0, init=0;
+            if(typeof(l)=='string'){v = l; n = r;}
+            else { v = r; n = l;}
+            if(n<0){n=-n;neg=1};
+            while(n){
+                const lowbit = n & (-n);
+                if(lowbit == 1){
+                    if(init == 0){
+                        init = 1;
+                        ctx.buffer.push(`    add ${sr}, $zero, ${v}`);
+                    }else{
+                        ctx.buffer.push(`    add ${sr}, ${sr}, ${v}`);
+                    }
+                }
+                else{
+                    ctx.buffer.push(`    sll ${tr}, ${v}, ${Math.log(lowbit)/Math.log(2)}`);
+                    if(init == 0){
+                        init = 1;
+                        ctx.buffer.push(`    add ${sr}, $zero, ${tr}`);
+                    }
+                    else{
+                        ctx.buffer.push(`    add ${sr}, ${sr}, ${tr}`);
+                    }
+                }
+                n-=lowbit;
+            }
+            if(neg){
+                ctx.buffer.push(`    nor ${sr}, ${sr}, $zero`);
+            }
+            freeRegister(ctx, tr);
+            freeRegister(ctx, v);
+            return sr;
+        }
+    }
+    else if(ast[0] == 'a'){
+        if(typeof(l) == 'number')return null;
+        let dst = l;
+        if(ctx.exptregu[l] != true){
+            dst = applyRegister(ctx);
+        }
+        if(typeof(r) == 'string'){
+            ctx.buffer.push(`    add ${dst}, ${l}, ${r}`);
+            ctx.buffer.push(`    lw ${dst}, 0(${dst})`);
+        }
+        else{
+            ctx.buffer.push(`    lw ${dst}, ${r}(${l})`);
+        }
+        return dst;
+    }
+    else if(ast[0] == '='){
+        if(typeof(r) == 'string'){
+             ctx.buffer.push(`    add ${l}, $zero, ${r}`);
+             freeRegister(ctx, r);
+        }
+        else{
+             ctx.buffer.push(`    addi ${l}, $zero, ${r}`);
+        }
+    }
+    else{
+        if(typeof(l) == 'string' && typeof(r) == 'string'){
+            freeRegister(ctx, r);
+            let dst = l;
+            if(ast[0] == '>'){
+                ctx.buffer.push(`    slt ${dst}, ${r}, ${l}`);
+            }
+            else{
+                ctx.buffer.push(`    ${operatorMap[ast[0]]} ${dst}, ${l}, ${r}`);
+            }
+            if(ctx.exptregu[l] != true){
+                const dst = applyRegister(ctx);
+            }
+            return dst;;
+        }
+        else if(typeof(l) == 'string' && typeof(r) == 'number'){
+            let dst = l;
+            if(ctx.exptregu[l] != true){
+                dst = applyRegister(ctx);;
+            }
+            if(ast[0] == '-'){
+                ctx.buffer.push(`    addi ${dst}, ${l}, ${-r}`);
+            }
+            else if(ast[0] == '>'){
+                ctx.buffer.push(`    slti ${dst}, ${l}, ${r}`);
+                ctx.buffer.push(`    xor ${dst}, ${dst}, 1`);
+            }
+            else{
+                ctx.buffer.push(`    ${operatorMap[ast[0]]}i ${dst}, ${l}, ${r}`);
+            }
+            return dst;
+        }
+        else if(typeof(l) == 'number' && typeof(r) == 'string'){
+            let dst = r;
+            if(ctx.exptregu[r] != true){
+                dst = applyRegister(ctx);;
+            }
+            if(ast[0] == '-'){
+                ctx.buffer.push(`    addi ${dst}, ${r}, ${-l}`);
+                ctx.buffer.push(`    sub ${dst}, $zero, ${dst}`);
+            }
+            else if(ast[0] == '>'){
+                 ctx.buffer.push(`    slti ${dst}, ${r}, ${l}`);
+                 ctx.buffer.push(`    xor ${dst}, ${dst}, 1`);
+            }
+            else{
+                ctx.buffer.push(`    ${operatorMap[ast[0]]}i ${dst}, ${r}, ${l}`);
+            }
+            return dst;
+        }
+        else{
+            return eval(`l${ast[0]}r`);
+        }
+    }
+}
+
+function dealExpression(ctx, exp){
+    const ast = parseAssignment(ctx, tokenize(exp));
+    console.log(pr(ast));
+    return generateExpression(ctx, ast);
+}
+
+exports.dealExpression = dealExpression;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+
+// unreachable code
+// repeat label
+// useless j
+// useless move
+// continuous optimiztion
+function optimize(code){
+    let result = [];
+    const result2 = [];
+    const replaceTable = {};
+    let waste = 0;
+    let onLabel = null;
+    let jname = null;
+    for(let i = 0; i < code.length; i++){
+        if(/^\s*$/.test(code[i])){result.push(code[i]);continue;}
+        if(/add\s/.test(code[i])){
+            const tuple = 
+            /add\s*([a-zA-Z0-9$_]*)\s*,\s*([a-zA-Z0-9$_]*)\s*,\s*([a-zA-Z0-9$_]*)/.exec(code[i]);
+            //console.log(tuple);
+            if(tuple[2] == '$zero'){
+                if(tuple[3] == tuple[1])continue;
+            }
+            if(tuple[3] == '$zero'){
+                if(tuple[2] == tuple[1])continue;
+                continue;
+            }
+        }
+        if(/^\s*j\s+/.test(code[i])){
+            jname = /j\s*([a-zA-Z_$0-9]*)\s*/.exec(code[i])[1];
+            result.push(code[i]);
+            waste = 1;
+            continue;
+        }
+        if(code[i].indexOf(':') != -1){
+            waste = 0;
+            const name = /\s*([a-zA-Z_$0-9]+)\s*:/.exec(code[i]);
+            if(name[1] == jname)result.pop();
+        }
+        jname = null;
+        if(waste != 1){
+            result.push(code[i]);
+        }
+    }
+    for(let i = 0; i < result.length; i++){
+        if(result[i].indexOf(':') != -1){
+            if(onLabel){
+                const name = /\s*([a-zA-Z_$0-9]+)\s*:/.exec(result[i]);
+                replaceTable[name] = onLabel;
+                continue;
+            }else{
+                const name = /\s*([a-zA-Z_$0-9]+)\s*:/.exec(result[i]);
+                onLabel = name[1];
+                result2.push(result[i]);
+            } 
+        }else{
+            onLabel = null;
+            result2.push(result[i]);
+        }
+    }
+    result = [];
+    for(let i = 0; i < result2.length; i++){
+        for(let key of Object.keys(replaceTable)){
+            result2[i] = result2[i].replace(key, replaceTable[key]);
+        }
+        result.push(result2[i]);
+    }
+    return result;
+}
+
+exports.optimize = optimize;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
 /*
 lw, sw, addi, jr, jal, j
 */
@@ -14,8 +499,8 @@ const script = `
     
 `;
 
-const expression = require('./expression');
-const common = require('./common');
+const expression = __webpack_require__(1);
+const common = __webpack_require__(0);
 const applyRegister = common.applyRegister;
 const freeRegister = common.freeRegister;
 const lookup = common.lookup;
@@ -563,7 +1048,7 @@ function preprocess(script, error){
     return ctx.result;
 }
 console.log(script);
-const optimizer = require('./optim');
+const optimizer = __webpack_require__(2);
 // let isError = false;
 // const data = preprocess(script, (i, text)=>{console.log(`ERROR at ${i}: ${text}`);isError=true;});
 // let result = optimizer.optimize(data);
@@ -579,3 +1064,6 @@ window.utils = {
 };
 
   
+
+/***/ })
+/******/ ]);
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: './trans.js',
+  output: {
+    filename: 'bundle.js'
+  }
+}
